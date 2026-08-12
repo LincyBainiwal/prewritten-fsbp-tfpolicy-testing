254691dd8a4505ab221f81d7f86c92458d73c222
Lincy Bainiwal <Lincy.Bainiwal@ibm.com>
2026-08-12 13:35:23 +0530

    add kinesis , kms

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 terraform {
+  required_version = ">= 1.15.0"
+
   cloud {
+    
     organization = "nagateja-test-org"
+
     workspaces {
-      name = "prewritten-testing"
+      name = "prewritten-fsbp-testing"
     }
   }
-
-  
 }
 
 provider "aws" {
@@ -25,5 +27,19 @@ resource "aws_kinesis_stream" "example" {
   retention_period = 168                  # checked
   encryption_type  = "KMS"               # checked
   kms_key_id       = "alias/aws/kinesis"  # checked
+}
+
+
+# ============================================================
+# KMS
+# Policies check: deletion_window_in_days, is_enabled,
+#                 enable_key_rotation
+# ============================================================
+
+resource "aws_kms_key" "example" {
+  description             = "example-kms-key"
+  enable_key_rotation     = true          # checked
+  is_enabled              = true          # checked
+  deletion_window_in_days = 30            # checked
 }
 
